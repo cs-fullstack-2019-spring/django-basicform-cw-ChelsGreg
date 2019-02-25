68e7e5bb7765890a96468a2ad0a3b296e94c7706
ChelsGreg <cgreg2217@gmail.com>
2019-02-25 12:39:20 -0600

    Complete

--- accountApp/urls.py
+++ accountApp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("addFive/", views.addFive, name="addFive"),
+    path("printAccount/", views.printAccount, name="printAccount"),
+    path("", views.index, name="index"),
 ]
--- accountApp/views.py
+++ accountApp/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from .models import Account
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Test URL")
+    allAccount = Account.objects.all()
+
+    context = {
+        "allAccounts": allAccount
+    }
+    return render(request, 'accountApp/index.html', context)
+
+
+def printAccount(request):
+    print( request.POST)
+
+    return HttpResponse("Welcome to " + request.POST['printAccount'] + "s Account")
+
+
+def addFive(request, accountID):
+    thisAccount = get_object_or_404(Account, pk=accountID)
+    thisAccount.checking += 5
+    thisAccount.save()
+    return redirect("index")
+
